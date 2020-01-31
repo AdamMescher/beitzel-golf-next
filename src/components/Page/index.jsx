3954a03d19c4b0f33f4d4d9f02dda847377eab9c
Adam Mescher <admescher@gmail.com>
2020-01-30 17:26:58 -0700

    Remove unused Component import in Page component

--- src/components/Page/index.jsx
+++ src/components/Page/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../GlobalStyle';
 import StyledPage from './styled';
