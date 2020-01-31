8e2b2688eef6b549246b643fd329527577dcc4f2
Adam Mescher <admescher@gmail.com>
2020-01-30 17:05:23 -0700

    Create Inner component with boilerplate index.jsx and styled.jsx

---
+++ src/components/Inner/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import StyledInner from './styled';
+
+const Inner = () => (
+  <StyledInner />
+);
+
+export default Inner;
---
+++ src/components/Inner/styled.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+const StyledInner = styled.div``;
+
+export default StyledInner;
